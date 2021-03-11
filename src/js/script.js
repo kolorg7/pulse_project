64bb01fcaad16960277ef7f22852131b4da1a95e
kolorg7 <pavelkorol3v@gmail.com>
2021-03-11 17:34:29 +0300

    fix catalog animation

--- src/js/script.js
+++ src/js/script.js
@@ -17,10 +17,13 @@ $(document).ready(function(){
         ]
     });
 
+    // Catalog and tabs
+
     $('ul.catalog__tabs').on('click', 'li:not(catalog__tab_active)', function() {
         $(this)
             .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
             .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+        $('.catalog-item').removeAttr('style');
     });
 
     function toggleSlide(item) {
